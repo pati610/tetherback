7e89261a643fa120259182f7274b5dbbcce5b092
Dan Lenski <dlenski@gmail.com>
2016-06-07 08:54:40 -0500

    always print adb version (helps with bug reports)

--- tetherback/tetherback.py
+++ tetherback/tetherback.py
@@ -65,7 +65,7 @@ elif adbversion<(1,0,32) and args.transport==adbxp.pipe_xo:
     print("WARNING: exec-out pipe (--exec-out) probably won't work with ADB version < 1.0.32 (you have %s)" % adbversions, file=stderr)
 elif sys.platform!='linux2' and args.transport==adbxp.pipe_bin:
     print("WARNING: binary pipe (--pipe) will PROBABLY CORRUPT DATA on non-Linux host", file=stderr)
-elif args.verbose:
+else:
     print("Found ADB version %s" % adbversions, file=stderr)
 
 ########################################
